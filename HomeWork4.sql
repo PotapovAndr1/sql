dd3f85732a0977f56820baaf66efe5dae8ec1855
PotapovAndr <potapovandr@gmail.com>
2025-04-09 12:22:30 +0900

    Добавил песни

--- HomeWork4.sql
+++ HomeWork4.sql
@@ -25,40 +25,9 @@ INSERT INTO tracks (id, name, duration, album_id) VALUES
 (4, 'Topoliniy puh', 223, 2),
 (5, 'Do skoroy vstrechi', 201, 3),
 (6, 'Tramvai', 281, 3),
-
--- Сборники
-INSERT INTO collections (id, name, release_year) VALUES
-(1, 'Greatest Hits 2000s', 2010),
-(2, 'Hits 2000', 2016),
-(3, 'Best Rock', 2019),
-(4, 'Love Songs', 2015);
--- Исполнители
-INSERT INTO artists (id, name) VALUES
-(1, 'Leningrad'),
-(2, 'Ivanushki int'),
-(3, 'Zveri'),
-(4, 'Valeria');
-
--- Жанры
-INSERT INTO genres (id, name) VALUES
-(1, 'Pop'),
-(2, 'Rock'),
-(3, 'Alternative');
-
--- Альбомы
-INSERT INTO albums (id, name, release_year) VALUES
-(1, 'WWW', 2008),
-(2, 'Tuchi', 2000),
-(3, 'Chasiki', 2005);
-
--- Треки
-INSERT INTO tracks (id, name, duration, album_id) VALUES
-(1, 'WWW', 186, 1),
-(2, 'Terminator', 177, 1),
-(3, 'Tuchi', 295, 2),
-(4, 'Topoliniy puh', 223, 2),
-(5, 'Do skoroy vstrechi', 201, 3),
-(6, 'Tramvai', 281, 3);
+(7, 'Eto moy dom', 250, 1),
+(8, 'This is my song', 230, 2),
+(9, 'Gigant', 305, 3);
 
 -- Сборники
 INSERT INTO collections (id, name, release_year) VALUES
@@ -89,26 +58,29 @@ INSERT INTO collection_track (collection_id, track_id) VALUES
 (2, 4),
 (3, 1),
 (3, 5),
+(3, 7),
 (4, 4),
-(4, 6);
+(4, 6),
+(4, 8);
 
--- Название и продолжительность самого длительного трека
+
+-- 1. Название и продолжительность самого длительного трека
 SELECT name, duration
 FROM tracks
 ORDER BY duration DESC
 LIMIT 1;
 
--- Название треков, продолжительность которых не менее 3,5 минут (210 секунд)
+-- 2. Название треков, продолжительность которых не менее 3,5 минут (210 секунд)
 SELECT name
 FROM tracks
 WHERE duration >= 210;
 
--- Названия сборников, вышедших в период с 2018 по 2020 год включительно
+-- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно
 SELECT name
 FROM collections
 WHERE release_year BETWEEN 2018 AND 2020;
 
--- Исполнители, чьё имя состоит из одного слова
+-- 4. Исполнители, чьё имя состоит из одного слова
 SELECT name
 FROM artists
 WHERE name NOT LIKE '% %';
